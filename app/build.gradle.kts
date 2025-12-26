c08e53a540721700154781b112729630a54843ea
马宁宁 <maningning@sinosoft.com.cn>
2025-12-26 09:25:45 +0800

    生成room数据库结构

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
+    ksp {
+        arg("room.schemaLocation", "$projectDir/schemas")
+    }
 }
 
 dependencies {
